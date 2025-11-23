1b905f3047d35e11d9d6fbf027446d3d628a8697
teetee971 <thierrynaud2009@gmail.com>
2025-11-23 15:09:54 -0400

    Add files via upload

--- BC_ELITE_project/app/page.tsx
+++ BC_ELITE_project/app/page.tsx
@@ -1,19 +1,2 @@
-import Hero from "../components/Hero";
-import SentinelCards from "../components/SentinelCards";
-import Pegasus from "../components/Pegasus";
-import VanguardModule from "../components/VanguardModule";
-import Backend from "../components/Backend";
-import Comparison from "../components/Comparison";
-
-export default function Page() {
-  return (
-    <div className="flex w-full flex-col gap-24">
-      <Hero />
-      <SentinelCards />
-      <Pegasus />
-      <VanguardModule />
-      <Backend />
-      <Comparison />
-    </div>
-  );
-}
+// BC-ELITE placeholder page
+export default function Page(){ return (<div>BC-ELITE</div>) }
